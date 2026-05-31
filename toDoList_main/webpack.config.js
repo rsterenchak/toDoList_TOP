285f4d0aa39862a98adcebb1297ed833a794bd8b
rsterenchak <rsterenchak@gmail.com>
2026-05-31 11:40:24 +0000

    modified filename/rebuild

--- toDoList_main/webpack.config.js
+++ toDoList_main/webpack.config.js
@@ -51,7 +51,7 @@ module.exports = {
     }),
   ],
   output: {
-    filename: '[name]bundle.js',
+    filename: '[name].[contenthash].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
     publicPath: 'auto',
